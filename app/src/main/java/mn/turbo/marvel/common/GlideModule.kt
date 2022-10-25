9039f054b6f1fa59df924d82a7f9305862341f41
turuuboldoo <turuu.boldoo@gmail.com>
2022-10-25 14:44:58 +0800

    color palette

--- app/src/main/java/mn/turbo/marvel/common/GlideModule.kt
+++ app/src/main/java/mn/turbo/marvel/common/GlideModule.kt
@@ -1,13 +1,27 @@
 package mn.turbo.marvel.common
 
+import android.content.Context
+import android.util.Log
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.module.AppGlideModule
 import com.bumptech.glide.request.RequestOptions
 import com.bumptech.glide.request.transition.DrawableCrossFadeFactory
 import mn.turbo.marvel.R
+
+@com.bumptech.glide.annotation.GlideModule
+class GlideModule : AppGlideModule() {
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        super.applyOptions(context, builder)
+        builder.setLogLevel(Log.WARN)
+        builder.setDiskCache(InternalCacheDiskCacheFactory(context))
+    }
+}
 
 private val factory = DrawableCrossFadeFactory
     .Builder()
--- app/src/main/java/mn/turbo/marvel/presenter/movie/viewmodel/MovieDetailViewModel.kt
+++ app/src/main/java/mn/turbo/marvel/presenter/movie/viewmodel/MovieDetailViewModel.kt
@@ -2,10 +2,12 @@ package mn.turbo.marvel.presenter.movie.viewmodel
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import mn.turbo.marvel.common.Constant
 import mn.turbo.marvel.common.Resource
@@ -45,6 +47,6 @@ class MovieDetailViewModel @Inject constructor(
                     )
                 }
             }
-        }
+        }.launchIn(viewModelScope)
     }
 }
