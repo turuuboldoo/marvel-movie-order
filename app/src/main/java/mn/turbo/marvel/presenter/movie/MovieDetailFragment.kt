60af7120046dfa9d35d1fd6050d8b7ca4b4eb58b
turuuboldoo <turuu.boldoo@gmail.com>
2022-11-02 13:00:11 +0800

    image rounded corner

--- app/src/main/java/mn/turbo/marvel/presenter/movie/MovieDetailFragment.kt
+++ app/src/main/java/mn/turbo/marvel/presenter/movie/MovieDetailFragment.kt
@@ -10,12 +10,12 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.math.roundToInt
 import mn.turbo.marvel.R
 import mn.turbo.marvel.common.collectLatestLifecycleFlow
 import mn.turbo.marvel.common.cropTop
 import mn.turbo.marvel.databinding.FragmentMovieDetailBinding
 import mn.turbo.marvel.presenter.movie.viewmodel.MovieDetailViewModel
-import kotlin.math.roundToInt
 
 @AndroidEntryPoint
 class MovieDetailFragment : Fragment(), OnClickListener {
