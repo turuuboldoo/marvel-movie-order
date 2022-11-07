c1350893d0f971f3a8e976cdfa1776a34298cedc
turuuboldoo <turuu.boldoo@gmail.com>
2022-11-07 10:14:57 +0800

    movie list grid layout

--- app/src/main/java/mn/turbo/marvel/presenter/movie/MovieListFragment.kt
+++ app/src/main/java/mn/turbo/marvel/presenter/movie/MovieListFragment.kt
@@ -4,14 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.ViewCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import androidx.viewpager2.widget.ViewPager2
 import dagger.hilt.android.AndroidEntryPoint
-import mn.turbo.marvel.R
 import mn.turbo.marvel.common.collectLatestLifecycleFlow
 import mn.turbo.marvel.databinding.FragmentMovieListBinding
 import mn.turbo.marvel.presenter.movie.adapter.MovieAdapter
@@ -43,23 +40,7 @@ class MovieListFragment : Fragment() {
             )
         }
 
-        val pageMarginPx = resources.getDimensionPixelOffset(R.dimen.pageMargin)
-        val offsetPx = resources.getDimensionPixelOffset(R.dimen.offset)
-
-        binding.viewPager.apply {
-            adapter = movieAdapter
-            offscreenPageLimit = 3
-
-            setPageTransformer { page, position ->
-                val viewPager = page.parent.parent as ViewPager2
-                val offset = position * -(2 * offsetPx + pageMarginPx)
-                if (ViewCompat.getLayoutDirection(viewPager) == ViewCompat.LAYOUT_DIRECTION_RTL) {
-                    page.translationX = -offset
-                } else {
-                    page.translationX = offset
-                }
-            }
-        }
+        binding.recyclerView.adapter = movieAdapter
 
         collectLatestLifecycleFlow(viewModel.movieListState) { state ->
             binding.progressBar.isVisible = state.isLoading
