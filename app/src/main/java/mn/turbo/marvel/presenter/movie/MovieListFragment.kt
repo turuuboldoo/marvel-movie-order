c0261bbe102fadb345837ee771cd167191cb1bdf
turuuboldoo <turuu.boldoo@gmail.com>
2022-11-07 11:47:40 +0800

    filter ???

--- app/src/main/java/mn/turbo/marvel/presenter/movie/MovieListFragment.kt
+++ app/src/main/java/mn/turbo/marvel/presenter/movie/MovieListFragment.kt
@@ -1,21 +1,25 @@
 package mn.turbo.marvel.presenter.movie
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
+import mn.turbo.marvel.R
 import mn.turbo.marvel.common.collectLatestLifecycleFlow
 import mn.turbo.marvel.databinding.FragmentMovieListBinding
 import mn.turbo.marvel.presenter.movie.adapter.MovieAdapter
 import mn.turbo.marvel.presenter.movie.viewmodel.MovieViewModel
 
 @AndroidEntryPoint
-class MovieListFragment : Fragment() {
+class MovieListFragment : Fragment(), MenuProvider {
 
     private var _binding: FragmentMovieListBinding? = null
     private val binding get() = _binding!!
@@ -33,6 +37,12 @@ class MovieListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        (requireActivity() as AppCompatActivity)
+            .apply {
+                setSupportActionBar(binding.toolbar)
+                supportActionBar?.title = ""
+            }
+
         val movieAdapter = MovieAdapter { movie ->
             findNavController().navigate(
                 MovieListFragmentDirections
@@ -46,10 +56,31 @@ class MovieListFragment : Fragment() {
             binding.progressBar.isVisible = state.isLoading
             movieAdapter.submitList(state.data)
         }
+
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(
+            this,
+            viewLifecycleOwner,
+            Lifecycle.State.RESUMED
+        )
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.movie_menu, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return when (menuItem.itemId) {
+            R.id.menuFilter -> {
+                Toast.makeText(requireContext(), "menu filter", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> false
+        }
     }
 }
